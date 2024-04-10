5a03872f32f72027a4403a5dbbc622c42af35986
Puleng75 <133369426+Puleng75@users.noreply.github.com>
2024-04-10 15:05:06 +0200

    added new files-html and css files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import Weather from "./Weather.js";
-
-
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 function App() {
 
   return (
     <div className="App">
+      <div className="container">
     <h1>Weather App</h1>
     <Weather/>
-    <br/>
-
-    <p>⛈15°C</p>
+  
       <footer>This project was coded by {" "}<a href="https://www.puleng.com" target="_blank" rel="noreferrer">Puleng Dlamini</a>and is
         {""} <a href="https://github.com/puleng75/WeatherAppReact" target="_blank" rel="noreferrer">Open-sourced on Github</a>
 </footer>
+    </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,50 @@
 
-import React,{useState} from "react";
+import React from "react";
+import Weather from "./Weather.css";
 
-
-export default function Weather() {
-    let [setCity] = useState("");
-
-
-function handleSubmit(event) {
-    event.preventDefault();
-    alert(`15°C`);
-}
-
-function updateCity(event){
-    setCity(event.target.value);
-}
+export default function WeatherAppReacteather() {
+    
 
 return(
-    <form onSubmit={handleSubmit}>
-        <input type="search" onChange={updateCity}/>
-        <input type="submit" value="Search" />
+    <div className="weather">
+        <form>
+            <div className="row">
+            <div className="col-9">
+            <input type="search" 
+            placeholder="Search city"
+            className="form-control"
+            autoFocus="on"/>
+             </div>
+             <div className="col-3">
+            <input type="submit"
+             placeholder="Search" className="btn btn-primary w-100" />
+             </div> 
+             </div>
         </form>
+
+    <h1>Pretoria</h1>
+    <ul>
+<li>Tuesday 16:00</li>
+<li>Sunny</li>
+    </ul>
+    <div className="row mt-3">
+        <div className="col-6">
+            <div className="clearfix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"alt="sunny"
+            className="float-left"/>
+            <div className="float-left">
+           <span className="temperature"> 24</span><span className="unit">°C</span>
+           </div>
+           </div>
+        </div>
+        <div className="col-6">
+        <ul>
+<li>Precipitation:1%</li>
+<li>Humidity:34%</li>
+<li>Wind:23km/h</li>
+    </ul> 
+        </div>
+    </div>
+</div>
 );
 }
